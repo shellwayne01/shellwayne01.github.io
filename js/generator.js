5e48fae887fd75e883917e31ac2b8e5e4261ae73
shellwayne01 <shellwayne01@gmail.com>
2019-06-05 10:18:36 -0400

    updated

--- js/generator.js
+++ js/generator.js
@@ -1,3 +1,4 @@
+//Greeting generator
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //inclusive
 }
@@ -5,8 +6,22 @@ function getRandomInt(min, max) {
 function greetingGenerator(){
   var x = getRandomInt(0,4);
   var names = ["tasty jelly bean", "salty donut", "poppin popcorn kernel", "spicy salmon roll", "gooey butter cake"]
-  var greeting = "Hello, you " + names[x] + "!";
-  return greeting;
-}
+  // var greeting = "Hello, you " + names[x] + "!";
+  var chosen = names[x]
+  var txt = '';
+  var wait = 0;
 
-document.getElementById("greeting").innerHTML = greetingGenerator();
+  tick = function() {
+      txt = chosen.substring(0, txt.length + 1);
+      document.getElementById("typewriter").innerHTML = txt;
+      if (txt.length < chosen.length){
+        doSetTimesout();
+      }
+  }
+  function doSetTimesout(){
+    setTimeout(function() {tick(); }, 150 );
+  }
+  // document.getElementById("greeting").innerHTML = txt;
+  // return greeting;
+  doSetTimesout();
+}
